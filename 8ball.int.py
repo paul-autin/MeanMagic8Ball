a7501236b22521e1bde08de158deb90e45cb9b7c
paul-autin <paul.autin.zoso@gmail.com>
2025-03-12 14:47:50 -0500

    Create 8ball.int.py

---
+++ 8ball.int.py
@@ -0,0 +1,44 @@
+import random
+random_number = random.randint(1,10)
+#print(random_number)
+
+name = "Paulie"
+question = "Will I ever get a job in tech?"
+answer = ""
+
+
+if random_number == 1:
+    answer = "Back off, I need space"
+elif random_number == 2:
+    answer = "You're such a little fat girl"
+elif random_number == 3:
+    answer = "Leave me alone; I'm expressing myself"
+elif random_number == 4:
+    answer = "Global warming gives me the ick"
+elif random_number == 4:
+    answer = "Try again later"
+elif random_number == 5:
+    answer = "Fine, whatever"
+elif random_number == 6:
+    answer = "Okay, I can see that working out"
+elif random_number == 7:
+    answer = "I would love that for you... NOT!"
+elif random_number == 8:
+    answer = "You should ask me in about 4 hours, I'll be sober by then"
+elif random_number == 9:
+    answer = "*Free Bird plays in the background while I ram my Dodge Durango into a family of four*"
+elif random_number == 10:
+    answer = "Maybe instead of playing with another man's Python script, you should go touch grass"
+elif random_number == 11:
+    answer = "Dude I'm WAYYY too drunk for this rn"
+elif random_number == 12:
+    answer = "Okay, I'll help you out if you can get my ankle monitor off"
+elif random_number == 13:
+    answer = "I hate that question almost as much as I hate you"
+elif random_number == 14:
+    answer = "It turns out that there aren't any hot singles in your area"
+elif random_number == 15:
+    answer = "Nooooo, you're so ugly when you cry..."
+
+print(name + " asks: " + question)
+print("Mean Magic 8 Ball's answer: " + answer)
